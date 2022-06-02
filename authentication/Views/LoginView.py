1e2070793e11f54c6b7259e660f9288cade1be84
9u4a <99junsung@gmail.com>
2022-06-02 16:17:08 +0900

    Implement LoginView POST Method

--- authentication/Views/LoginView.py
+++ authentication/Views/LoginView.py
@@ -1,9 +1,39 @@
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from ..models import User
 
 
 class LoginView(APIView):
     def post(self, request):
         data = JSONParser().parse(request)
-        return JsonResponse()
+        user = User.objects.get(user_id=data['user_id'])
+        print(user)
+        if not user:
+            response = {
+                'success': False,
+                'err': '존재하지 않는 id입니다.'
+            }
+            return JsonResponse(response, status=400)
+        if data['user_pw'] != user.user_pw:
+            response = {
+                'success': False,
+                'err': '비밀번호가 일치하지 않습니다.'
+            }
+            return JsonResponse(response, status=400)
+
+        token = RefreshToken.for_user(user)
+        refresh_token = str(token)
+        access_token = str(token.access_token)
+        response = JsonResponse({
+            'success': True,
+            'jwt_token': {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }
+        }, status=200)
+        response.set_cookie('access_token', access_token, httponly=True)
+        response.set_cookie('refresh_token', refresh_token, httponly=True)
+        return response
